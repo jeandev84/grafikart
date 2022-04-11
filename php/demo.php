9d92da4f05d081c325ddef563315702309a579a6
jeandev84 <jeandev84@yahoo.com>
2022-04-11 06:09:33 +0300

    creneau magasin

--- php/demo.php
+++ php/demo.php
@@ -27,6 +27,7 @@ $creneaux = [
 */
 
 
+/*
 $creneaux = [];
 
 while (true) {
@@ -45,8 +46,9 @@ while (true) {
 }
 
 
-/* print_r($creneaux); */
+print_r($creneaux);
 
+// Afficher le magasin  est ouvert de 14h a 18h et de 9h a 12h
 $heure = (int) readline("A quelle heure voulez vous visiter le magasin ? : ");
 
 $creneauTrouve = false;
@@ -60,6 +62,60 @@ foreach ($creneaux as $creneau) {
 
 if ($creneauTrouve) {
    echo "Le magasin sera ouvert";
+} else {
+    echo "Desole, le magasin sera ferme :(";
+}
+
+echo "\n";
+
+*/
+
+
+$creneaux = [];
+
+while (true) {
+    $debut = (int) readline("Heure d' ouverture : ");
+    $fin   = (int) readline("Heure de fermeture : ");
+
+    if($debut > $fin) {
+        echo "Le creneaux ne peut pas etre enregistre car l' heure d' ouverture ($debut) est superieur a l' heure de fermeture ($fin) \n";
+    } else {
+        $creneaux[] = [$debut, $fin];
+        $action = readline("Voulez vous enregister un nouveau creneau (o/n): ");
+        if ($action === 'n') {
+            break;
+        }
+    }
+}
+
+
+
+// Le magasin  est ouvert de 14h a 18h et de 9h a 12h
+
+echo "Le magasin est ouvert de";
+
+foreach ($creneaux as $index => $creneau) {
+   if ($index > 0) {
+       echo " et de ";
+   }
+   echo "{$creneau[0]}h a {$creneau[1]}h";
+}
+
+echo "\n";
+
+$heure = (int) readline("A quelle heure voulez vous visiter le magasin ? : ");
+
+$creneauTrouve = false;
+
+foreach ($creneaux as $creneau) {
+    if ($heure >= $creneau[0] && $heure <= $creneau[1]) {
+        $creneauTrouve = true;
+        break;
+    }
+}
+
+if ($creneauTrouve) {
+    echo "Le magasin sera ouvert";
 } else {
     echo "Desole, le magasin sera ferme :(";
 }

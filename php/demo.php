023da01af289d905bd4bcaf00026ef18ae1f5d12
jeandev84 <jeandev84@yahoo.com>
2022-04-11 06:56:37 +0300

    functions

--- php/demo.php
+++ php/demo.php
@@ -1,55 +1,22 @@
 <?php
 
-// Functions
+// array_push() ajoute des elements dans un tableau
+echo "Print notes\n";
+$notes   = [10, 20, 13];
+$notes[] = 16;
+array_push($notes, 9, 17);
+print_r($notes);
 
-$creneaux = [];
+// Renverse l'ordre d'un tableau sans modifier les cles
+echo "Reversed notes \n";
+$notes   = [10, 20, 13];
+$noteReversed = array_reverse($notes);
+print_r($notes);
+print_r($noteReversed);
 
-while (true) {
-    $debut = (int) readline("Heure d' ouverture : ");
-    $fin   = (int) readline("Heure de fermeture : ");
-
-    if($debut > $fin) {
-        echo "Le creneaux ne peut pas etre enregistre car l' heure d' ouverture ($debut) est superieur a l' heure de fermeture ($fin) \n";
-    } else {
-        $creneaux[] = [$debut, $fin];
-        $action = readline("Voulez vous enregister un nouveau creneau (o/n): ");
-        if ($action === 'n') {
-            break;
-        }
-    }
-}
-
-
-
-// Le magasin  est ouvert de 14h a 18h et de 9h a 12h
-
-echo "Le magasin est ouvert de";
-
-foreach ($creneaux as $index => $creneau) {
-   if ($index > 0) {
-       echo " et de ";
-   }
-   echo " {$creneau[0]}h a {$creneau[1]}h";
-}
-
-echo "\n";
-
-$heure = (int) readline("A quelle heure voulez vous visiter le magasin ? : ");
-
-$creneauTrouve = false;
-
-foreach ($creneaux as $creneau) {
-    if ($heure >= $creneau[0] && $heure <= $creneau[1]) {
-        $creneauTrouve = true;
-        break;
-    }
-}
-
-if ($creneauTrouve) {
-    echo "Le magasin sera ouvert";
-} else {
-    echo "Desole, le magasin sera ferme :(";
-}
-
-echo "\n";
+// Range un tableau du plus petit au plus grand
+echo "Sort notes (range by  asc) \n";
+$notes = [10, 20, 13, 9.7, 11];
+sort($notes);
+print_r($notes);
 

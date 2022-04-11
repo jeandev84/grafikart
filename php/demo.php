8394f771abcaeb59d1ce557beb079bff460fbcd6
jeandev84 <jeandev84@yahoo.com>
2022-04-11 05:08:46 +0300

    demo boucles

--- php/demo.php
+++ php/demo.php
@@ -1,3 +1,15 @@
 <?php
 
+/*
+ALGORITHM
+
+1. Demander a l' utilisateur  de rentrer une note ou taper "fin" s'il tape fin on ira a la suite
+2. Chaque note est sauvegardee dans un tableau $notes (pensez a la syntax $notes[])
+3. Si l' utilisateur tape fin on affiche le tableau des notes sous form de liste.
+*/
+
+
+$notes = [];
+
+$note  = readline('Entrez une note ou taper "fin" : ');
 
